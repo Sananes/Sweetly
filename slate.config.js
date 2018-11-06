a8e30f2fd5f22aac0ac1a0f612d2768a30815ccc
Aaron Sananes <aasananes@gmail.com>
2018-11-06 17:14:38 +0000

    Big update to header and hero

--- slate.config.js
+++ slate.config.js
@@ -12,6 +12,8 @@ module.exports = {
       alias: {
         jquery: path.resolve('./node_modules/jquery'),
         'lodash-es': path.resolve('./node_modules/lodash-es'),
+        'fonts': path.resolve('./src/assets/fonts'),
+        'images': path.resolve('./src/assets/images'),
       },
     },
   },
